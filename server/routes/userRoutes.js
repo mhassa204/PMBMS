3b7d7e9460e0eab053afb2d43fff992f6474547f
Muhammad Ahmad <ahmadcs442@gmail.com>
2024-01-03 14:08:36 +0500

    a

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const bcyrptjs = require("bcryptjs");
 const {
   getUsers,
   getUserById,
