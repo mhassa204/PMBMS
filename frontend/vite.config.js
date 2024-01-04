1a16a93e6907b26a931e9b34874e3c3f4d588cad
Muhammad Ali Hassan <mahassan@cuilahore.edu.pk>
2024-01-04 22:51:33 +0500

    Fix config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PMBMS/",
   resolve: {
     alias: {
       "@components": "/src/components",
